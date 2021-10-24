d3f6d8984476cf7bc69fb0199f8c5a121628dcbe
tom8hawk <grinly.ilpogo@gmail.com>
2021-10-24 04:48:35 +0300

    Adding asynchrony to a task that could cause the server to lag behind. Small code cleanup

--- src/main/java/ru/siaw/free/rpplugin/function/OnlineBook.java
+++ src/main/java/ru/siaw/free/rpplugin/function/OnlineBook.java
@@ -3,14 +3,11 @@ package ru.siaw.free.rpplugin.function;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.entity.EntityPortalExitEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.PlayerEditBookEvent;
@@ -18,39 +15,38 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
+import ru.siaw.free.rpplugin.RPplugin;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class OnlineBook implements Listener {
-    private static final List<OfflinePlayer> players = Arrays.asList(Bukkit.getOfflinePlayers());
-    private static boolean enabled, reset;
     private static String online, offline;
+    private static boolean enabled, reset;
+    private final RPplugin plugin = RPplugin.getInst();
+    private static final List<OfflinePlayer> players = Arrays.asList(Bukkit.getOfflinePlayers());
 
-    private static void update(ItemStack item) {
+    private void update(ItemStack item) {
         if (item != null && item.getType() == Material.WRITTEN_BOOK) { // проверяем подписанная ли это книга
-            BookMeta bookMeta = (BookMeta) item.getItemMeta(); // Получаем мету
-            if (bookMeta != null && bookMeta.getAuthor() != null) {
-                String authorName = extractAuthor(bookMeta).trim(); // достаем имя автора
-                if (enabled)
-                    bookMeta.setAuthor(authorName + " " + (Bukkit.getPlayer(authorName) != null ? online : offline)); // устанавливаем автора
-                else if (reset)
-                    bookMeta.setAuthor(authorName); // сбрасываем к обычному значению (просто автор книги)
-                item.setItemMeta(bookMeta); // устанавливаем мету
+            BookMeta book = (BookMeta) item.getItemMeta(); // Получаем мету
+            if (enabled && book != null && book.getAuthor() != null) {
+                Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+                    @Override
+                    public synchronized void run() {
+                        String author = book.getAuthor();
+                        for (OfflinePlayer p : players) { // проходим по всем игрокам сервера
+                            String name = p.getName();
+                            if (author.contains(name)) { // находим точный ник автора книги
+                                book.setAuthor(name + (!reset ? " " +
+                                        (Bukkit.getPlayer(name) != null ? online : offline) : "")); // устанавливаем автора
+                                break;
+                            }
+                        }
+                        Bukkit.getScheduler().runTask(RPplugin.getInst(), () -> item.setItemMeta(book)); // устанавливаем мету
+                    }
+                });
             }
         }
-    }
-
-    private static String extractAuthor(BookMeta meta) {
-        String author = meta.getAuthor();
-        for (OfflinePlayer p : players) {
-            String name = p.getName();
-            if (author.contains(name)) {
-                author = name;
-                break;
-            }
-        }
-        return author;
     }
 
     // присоединение к серверу
@@ -64,28 +60,8 @@ public class OnlineBook implements Listener {
     @EventHandler
     public void onEditBook(PlayerEditBookEvent e) {
         Player p = e.getPlayer();
-        if (e.isSigning() && !e.isCancelled() && !players.contains(p)) players.add(p);
-        for (ItemStack item : e.getPlayer().getInventory()) update(item);
-    }
-
-    // выход с портала
-    @EventHandler
-    public void onPortalExit(EntityPortalExitEvent e) {
-        Entity entity = e.getEntity();
-        if (entity instanceof Player) {
-            Player player = (Player) entity;
-            for (ItemStack item : player.getInventory()) update(item);
-        }
-    }
-
-    // подбор предмета
-    @EventHandler
-    public void onPickup(EntityPickupItemEvent e) {
-        Entity entity = e.getEntity();
-        if (entity instanceof Player) {
-            Player player = (Player) entity;
-            for (ItemStack item : player.getInventory()) update(item);
-        }
+        if (e.isSigning() && !players.contains(p)) players.add(p);
+        for (ItemStack item : p.getInventory()) update(item);
     }
 
     // открытие инвентаря (не инвентаря игрока)
